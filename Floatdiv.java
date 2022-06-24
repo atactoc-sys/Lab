79574fc1119ebef83626474f1f9f38da04264186
Saikat Mandi <104897172+atactoc-sys@users.noreply.github.com>
2022-06-24 11:16:41 +0530

    Create Floatdiv.java

---
+++ Floatdiv.java
@@ -0,0 +1,17 @@
+class Floating { public static void main(String[] args)
+
+{
+
+// fis to ensures that numbers are float data
+
+float fl = 10.5f
+
+float f2 = 20.6f
+
+// to store the multiplied value
+
+float f= fl* f2;
+
+// to print the answer System.out.println("The answer is: "+f);
+}
+}
