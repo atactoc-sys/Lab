8b56aeaeaae6c0c0d751bcc9ba7fa75815a83727
Saikat Mandi <104897172+atactoc-sys@users.noreply.github.com>
2022-06-24 11:15:17 +0530

    Create Add.java

---
+++ Add.java
@@ -0,0 +1,17 @@
+class Add{ void main(String args[]){
+
+public static
+
+int a=100;
+
+int b=10;
+
+// to store the addition value
+
+int c=(a+b);
+
+// to print the answer
+
+System.out.println("Addition of two numbers:" +c);
+}
+}
