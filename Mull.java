001f630ae48d427ebc5571479016a2af51557341
Saikat Mandi <104897172+atactoc-sys@users.noreply.github.com>
2022-06-24 11:12:24 +0530

    Create Mull.java

---
+++ Mull.java
@@ -0,0 +1,13 @@
+class Multi{ public static void main(String args[]){
+
+int a=100;
+
+int b=10;
+
+// to store the multiplied value int c=(a*b);
+
+// to print the answer
+
+System.out.println("Multiplicaition of two numbers:" +c);
+}
+}
